acf10b5d87d667b51e9b7dd9303aedd9f9576683
Ingo Kleiber <ikleiber@googlemail.com>
2021-04-24 17:26:21 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const RussianAlphabetApp = {
+    data() {
+      return {
+        questions_pool: {},
+        question_sets: [],
+        questions: [],
+        score: '0',
+        question: '',
+        guess: '',
+        solution: ''
+      }
+    },
+    created() {
+        fetch('./questions.json')
+            .then(response => response.json())
+            .then(obj => {
+                this.questions_pool = obj['questions']
+                this.question_sets = Object.keys(obj['questions'])
+
+                this.questions = this.questions_pool['characters']
+                this.question = _.sample(this.questions)
+                }
+            )
+    },
+    watch: {
+        guess: function() {
+            if (this.guess == this.question['transl']) {
+                this.score++
+                this.question = _.sample(this.questions)
+                this.guess = ''
+                this.solution = ''
+            }
+        }
+    },
+    methods: {
+        showSolution: function () {
+            this.solution = this.question['transl']
+        },
+        changeSet: function (event) {
+            this.questions = this.questions_pool[event.target.id]
+            this.question = _.sample(this.questions) 
+        }
+      }
+  }
+  
+Vue.createApp(RussianAlphabetApp).mount('#app')
